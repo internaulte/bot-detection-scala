37a20359dd09a701e4ec7fa80b2c05bb2f03bb99
lnault <laurent.nault@logpickr.com>
2021-07-28 00:27:01 +0200

    wip

--- src/main/scala/modules/bootstrap/Bootstrap.scala
+++ src/main/scala/modules/bootstrap/Bootstrap.scala
@@ -1,5 +1,26 @@
 package modules.bootstrap
 
+import modules.trafficloadbalancingmock.adapters.kafka.KafkaClientImpl
+import modules.trafficloadbalancingmock.adapters.kafka.interfaces.KafkaClient
+import modules.trafficloadbalancingmock.adapters.repositories.HttpTrafficSendRepositoryImpl
+import modules.trafficloadbalancingmock.adapters.services.TrafficLoadBalancingServiceImpl
+import modules.trafficloadbalancingmock.domain.usecases.HttpTrafficGenerationUseCases
+import modules.webserversmock.adapters.repositories.WebServerMockRepositoryImpl
+import modules.webserversmock.config.WebServersDataConfig
+import modules.webserversmock.domain.usecases.WebServerMockUseCases
+
 object Bootstrap {
-  def main(args: Array[String]): Unit = {}
+  def main(args: Array[String]): Unit = {
+
+    // Instances initialization
+    lazy val kafkaClient: KafkaClient = new KafkaClientImpl
+    lazy val httpTrafficSendRepository = new HttpTrafficSendRepositoryImpl(kafkaClient)
+    lazy val httpTrafficGenerationUseCases = new HttpTrafficGenerationUseCases(httpTrafficSendRepository)
+    lazy val trafficLoadBalancingService = new TrafficLoadBalancingServiceImpl(httpTrafficGenerationUseCases)
+    lazy val webServerMockRepository = new WebServerMockRepositoryImpl(trafficLoadBalancingService)
+    lazy val webServerMockUseCases = new WebServerMockUseCases(webServerMockRepository)
+
+    // Start sending Data in parallel for each list of logs:
+    WebServersDataConfig.webServersLogs.foreach(webServerMockUseCases.sendAllWebServerLogsToBotDetection)
+  }
 }
